4d9b0abe4cb82e8e75b516528afbd68a7e455de9
Arya Segara <aryasegara10@gmail.com>
2025-01-02 11:58:55 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+
+  return (
+    <>
+      <h1 className='text-danger'>Ecommers Full Stack</h1>
+    </>
+  )
+}
+
+export default App
